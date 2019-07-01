a3d9552a8eb4a5ba848ce8f0facc18dd08775363
icco22 <hodzic22@gmail.com>
2019-07-01 14:29:49 +0200

    Update TestController.java

--- src/test/java/com/companyname/projectname/Test/TestController.java
+++ src/test/java/com/companyname/projectname/Test/TestController.java
@@ -14,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
