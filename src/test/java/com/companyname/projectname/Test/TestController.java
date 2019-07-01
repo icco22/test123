6185d356a634c9f918c9521768796764ae6e09c0
icco22 <hodzic22@gmail.com>
2019-07-01 14:21:45 +0200

    Update TestController.java

--- src/test/java/com/companyname/projectname/Test/TestController.java
+++ src/test/java/com/companyname/projectname/Test/TestController.java
@@ -30,102 +30,24 @@ public class TestController extends Resources{
 
 	@Test
 	public void TestCaseController() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, IOException {
-		
-		String startTime = TestUtils.now("dd.MMMM.yyyy hh.mm.ss aaa");
-		ReportUtil.startTesting(System.getProperty("user.dir")+"//src//test//java//com//companyname//projectname//Reports//index.html", startTime, "Test", "1.5");
-		ReportUtil.startSuite("Suite1");
-		String TCStatus="Pass";
-		
-		// loop through the test cases
-		for(int TC=2;TC<=SuiteData.getRowCount("TestCases");TC++) {
-			
-			
-			
-			String TestCaseID = SuiteData.getCellData("TestCases", "TCID", TC);
-			String RunMode = SuiteData.getCellData("TestCases", "RunMode", TC);
-			
-			if(RunMode.equals("Y")) {
-				String TSStatus="Pass";
-				
-				dr = new FirefoxDriver();
-				driver = new EventFiringWebDriver(dr);
-				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-				
-				int rows = TestStepData.getRowCount(TestCaseID);
-				if(rows<2) { 
-					rows=2;
-				}
-				
-				// loop through test data
-				for(int TD=2;TD<=rows;TD++ ) {
-				
-					// loop through the test steps
-					System.out.println("SuiteData.getRowCount(TestCaseID)"+SuiteData.getRowCount(TestCaseID));
-					
-					for(int TS=2;TS<=SuiteData.getRowCount(TestCaseID);TS++) {
-						
-						keyword = SuiteData.getCellData(TestCaseID, "Keyword", TS);
-						webElement = SuiteData.getCellData(TestCaseID, "WebElement", TS);
-						ProceedOnFail = SuiteData.getCellData(TestCaseID, "ProceedOnFail", TS);
-						TSID = SuiteData.getCellData(TestCaseID, "TSID", TS);
-						Description = SuiteData.getCellData(TestCaseID, "Description", TS);
-						
-						TestDataField = SuiteData.getCellData(TestCaseID, "TestDataField", TS);
-						
-						
-						TestData = TestStepData.getCellData(TestCaseID, TestDataField, TD);	
-						
-						if(TestDataField.equals("email")){
-							TestData = "test"+System.currentTimeMillis()+"@gmail.com";
-						}
-						
-						
-						Method method = Keywords.class.getMethod(keyword);	
-						TSStatus = (String) method.invoke(method);
-						
-						if(TSStatus.contains("Failed")) {
-							// take the screenshot
-							String filename = TestCaseID+"["+(TD-1)+"]"+TSID+"["+TestData+"]";
-							TestUtils.getScreenShot(filename);
-							
-							TCStatus=TSStatus;
-							// report error
-							ReportUtil.addKeyword(Description, keyword, TSStatus, "Screenshot/"+filename+".jpg");
-
-							if(ProceedOnFail.equals("N")) {
-								break;
-							}
-						} else {
-							ReportUtil.addKeyword(Description, keyword, TSStatus, null);
-						}
-						
-						/*
-						if(ProceedOnFail.equals("N")) {
-							break;
-						}*/
-						
-						
-					}
-					ReportUtil.addTestCase(TestCaseID, startTime, TestUtils.now("dd.MMMM.yyyy hh.mm.ss aaa"), TCStatus);
-					driver.quit();
-				}
-			}else {
-				// skip the test case
-				ReportUtil.addTestCase(TestCaseID, startTime, TestUtils.now("dd.MMMM.yyyy hh.mm.ss aaa"), "Skipped");
-				driver.quit();
-			}
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\HP EliteBook 8560W\\Desktop\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String baseUrl = "http://localhost:9095//helloWorld";
+		driver.get(baseUrl);
+		Thread.sleep(3000);
+		WebElement helloWorld = driver.findElement(By.xpath("//h2[contains(text(),'Hello World!')]"));
+		if(helloWorld.isDisplayed()) {
+			System.out.println("Test prosao");
+		} else {
+			System.out.println("Test pao");
 		}
-
-		ReportUtil.endSuite();
-		ReportUtil.updateEndTime(TestUtils.now("dd.MMMM.yyyy hh.mm.ss aaa"));
-		
+		driver.quit();
 	}
-	
 	
 	@AfterClass
 	public void quitBrowser() {
-		System.out.println("In quitBrowser---------------------------");
-		driver.quit();
+		
 	}
 	
 	
